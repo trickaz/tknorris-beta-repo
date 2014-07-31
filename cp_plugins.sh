a190d5c607accb29b19e2f151c87b3e9283cc773
Tracy Norris <tknorris@gmail.com>
2014-07-31 15:49:58 -0400

    Added themepak to repo

--- cp_plugins.sh
+++ cp_plugins.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/1Channel/ ~/eclipseworkspace/tknorris-beta-repo/plugin.video.1channel
+rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/1channel.themepaks/ ~/eclipseworkspace/tknorris-beta-repo/script.1channel.themepak
