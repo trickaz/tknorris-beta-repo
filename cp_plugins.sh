0dfb577faafcdf732be022e51e5ea2549ccf89d4
Tracy Norris <tknorris@gmail.com>
2014-09-26 13:58:28 -0400

    Add 2.4.66 of 1CH to repo

--- cp_plugins.sh
+++ cp_plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/1Channel/ ~/eclipseworkspace/tknorris-beta-repo/plugin.video.1channel
-rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/salts/ ~/eclipseworkspace/tknorris-beta-repo/plugin.video.salts
+rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/1Channel/ ~/eclipseworkspace/tknorris-beta-repo/plugin.video.1channel
+#rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/salts/ ~/eclipseworkspace/tknorris-beta-repo/plugin.video.salts
 rsync --delete -av --exclude=".*" --exclude="*.pyc" ~/eclipseworkspace/1channel.themepaks/ ~/eclipseworkspace/tknorris-beta-repo/script.1channel.themepak
